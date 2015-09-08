53195c8dd1d1fb4549470c09a1b700e74fbb3589
Floffyko <florinko@gmail.com>
2015-09-08 15:01:43 +0100

    lang.php
    First Romanian translation.

---
+++ lang/ro/lang.php
@@ -0,0 +1,60 @@
+
+Skip to content
+This repository
+
+    Pull requests
+    Issues
+    Gist
+
+
+12
+12
+
+    15
+
+dokufreaks/plugin-blog
+
+plugin-blog/lang/en/lang.php
+@michitux michitux on Sep 2, 2013 Fix #40, use numbered placeholders in autoarchive translation
+
+3 contributors
+@michitux
+@splitbrain
+@dklight
+@Floffyko
+37 lines (32 sloc) 1.205 kB
+<?php
+/**
+ * Romanian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Esther Brunner <wikidesign@gmail.com>
+ */
+// custom language strings for the plugin
+$lang['blog']       = 'Blog';
+$lang['older']      = 'Intrări vechi';
+$lang['newer']      = 'Intrări mai noi';
+$lang['newentry']   = 'Inserează blog nou:';
+$lang['missing_includeplugin']  = 'Plugin-ul Include trebuie să fie instalat pentru ca blogul să fie funcțional.';
+$lang['missing_pagelistplugin'] = 'The Pagelist Plugin must be installed for archive lists to work.';
+$lang['missing_tagplugin']      = 'Plugin-ul Tag trebuie să fie instalat pentru a utiliza convocări subtile tag.';
+$lang['autoarchive'] = '%1$d Post(ări) pentru %2$lea %3$lea';
+$lang['month_01'] = 'Ianuarie';
+$lang['month_02'] = 'Februarie';
+$lang['month_03'] = 'Martie';
+$lang['month_04'] = 'Aprilie';
+$lang['month_05'] = 'Mai';
+$lang['month_06'] = 'Iunie';
+$lang['month_07'] = 'Iulie';
+$lang['month_08'] = 'August';
+$lang['month_09'] = 'Septembrie';
+$lang['month_10'] = 'Octobrie';
+$lang['month_11'] = 'Noiebrie';
+$lang['month_12'] = 'Decembrie';
+$lang['entries']       = 'intrări';
+$lang['entry']         = 'intrare';
+$lang['archive_title'] = 'Istoria blogului';
+
+    Status API Training Shop Blog About Pricing 
+
+    © 2015 GitHub, Inc. Terms Privacy Security Contact Help 
